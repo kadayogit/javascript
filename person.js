e687319c470df8a67bad2641f0cf47119bfa53ca
kadayogit <guledodowaa@gmail.com>
2024-04-27 18:18:44 +0300

    Added js modules examples

---
+++ person.js
@@ -0,0 +1,9 @@
+ // In-line individually:
+//  export const name = "Jesse";
+//  export const age = 40;
+
+ // All at once at the bottom:
+ const name = "Jesse";
+ const age = 40;
+
+ export {name, age};
